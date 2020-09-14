46daa6be60b7d7b441e623698436edd9811b90fb
Griffin Chure <gchure@caltech.edu>
2020-09-14 12:45:25 -0700

    Initial commit of pboc-py files

---
+++ pboc/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+PBoC-Py — A Python Module for Physical Biology 
+===============================================
+
+This module contains a variety of Python utilities frequently used in the lab. 
+This module is meant to *supplement* the specific code written for your research 
+project. 
+
+It is under development and is not yet ready for use. 
+
+"""
+
+
+# from . import mcmc
+from . import viz
+from . import flow
+from . import image
+from . import stats
+from . import thermo
+
+__author__ = """Griffin Chure"""
+__email__ = """griffinchure@gmail.com"""
+__version__ = "0.0.0"
