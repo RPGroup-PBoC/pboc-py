0ff65ba098a3195bbc7ed8c700798ae39f68ef3b
tomroesch <troeschi@caltech.edu>
2020-09-16 14:12:29 -0700

    Added minimal setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+with open("README", 'r') as f:
+    long_description = f.read()
+
+setup(
+   name='pboc',
+   version='0.0.1',
+   description='Common modules used in projects in the Phillips laboratory.',
+   license='MIT',
+   author='Tom Roeschinger, Griffin Chure',
+   author_email='troeschi@caltech.edu',
+   url="https://github.com/RPGroup-PBoC/pboc-py"
+)
